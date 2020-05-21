403407d48674e5d17cfb5e732ef02faeb26e8b2d
sweetgang <lider218@gmail.com>
2020-05-21 16:07:37 +0200

    v0.1.1 html step to end

---
+++ src/main/html/js/dishes.js
@@ -0,0 +1,211 @@
+
+var dishes = [
+{
+	dishName: 'Morning in Miami',
+	dishPrice: 21.9
+},
+{
+	dishName: 'Morning in Chicago',
+	dishPrice: 21.9
+},
+{
+	dishName: 'Morning in San Francisco',
+	dishPrice: 21.9
+},
+{
+	dishName: 'Morning in New Orleans',
+	dishPrice: 21.9
+},
+{
+	dishName: 'Morning in Los Angeles',
+	dishPrice: 21.9
+},
+{
+	dishName: 'Morning in New York',
+	dishPrice: 21.9
+},
+{
+	dishName: 'Koko Banana',
+	dishPrice: 15.9
+},
+{
+	dishName: 'Raspberry Cranberry',
+	dishPrice: 15.9
+},
+{
+	dishName: 'Spinach Avocado',
+	dishPrice: 15.9
+},
+{
+	dishName: 'Salmon salad',
+	dishPrice: 37
+},
+{
+	dishName: 'Salad with goat’s cheese',
+	dishPrice: 32
+},
+{
+	dishName: 'Chicken salad',
+	dishPrice: 36
+},
+{
+	dishName: 'Ceasar Salad with chicken',
+	dishPrice: 36
+},
+{
+	dishName: 'Caesar Salad with fried shrimps with garlic and chilli',
+	dishPrice: 45
+},
+{
+	dishName: 'Caesar Salad with grilled juicy beef sirloin',
+	dishPrice: 56
+},
+{
+	dishName: 'Beef Sirloin Salad',
+	dishPrice: 56
+},
+{
+	dishName: 'Mega Double Burger',
+	dishPrice: 65
+},
+{
+	dishName: 'Classic Burger',
+	dishPrice: 29
+},
+{
+	dishName: 'Chicken Burger',
+	dishPrice: 29
+},
+{
+	dishName: 'ROAD Burger',
+	dishPrice: 35
+},
+{
+	dishName: 'Cheeseburger',
+	dishPrice: 35
+},
+{
+	dishName: 'Thai Chicken Burger',
+	dishPrice: 31
+},
+{
+	dishName: 'Black Burger',
+	dishPrice: 39
+},
+{
+	dishName: 'White Chockolate Dessert',
+	dishPrice: 21
+},
+{
+	dishName: 'SOUFFLÉ',
+	dishPrice: 21
+},
+{
+	dishName: 'Coffee Brûlée',
+	dishPrice: 14
+},
+{
+	dishName: 'Water ROAD',
+	dishPrice: 10
+},
+{
+	dishName: 'Coca-cola, Coca-cola zero, Sprite, Fanta',
+	dishPrice: 9
+},
+{
+	dishName: 'Lemoniade',
+	dishPrice: 8
+},
+{
+	dishName: 'Cappy',
+	dishPrice: 6
+},
+{
+	dishName: 'JUICE MADE OF FRESH FRUIT (200ML)',
+	dishPrice: 8
+},
+{
+	dishName: 'JUICE MADE OF FRESH FRUIT (500ML)',
+	dishPrice: 16
+},
+{
+	dishName: 'Espresso',
+	dishPrice: 7
+},
+{
+	dishName: 'COFFEE',
+	dishPrice: 8
+},
+{
+	dishName: 'COCOFFEE with milkFFEE',
+	dishPrice: 9
+},
+{
+	dishName: 'Cappuccino',
+	dishPrice: 10
+},
+{
+	dishName: 'TEA',
+	dishPrice: 10
+},
+{
+	dishName: 'CONTENDA Chardonnay',
+	dishPrice: 14
+},
+{
+	dishName: 'CANEPA Classico Sauvignon Blanc',
+	dishPrice: 15
+},
+{
+	dishName: 'BACH EXTRISSIMO Brut Natur',
+	dishPrice: 75
+},
+{
+	dishName: 'ROPITEAU Grenache/Vermentino',
+	dishPrice: 75
+},
+{
+	dishName: 'ALBIZZIA Chardonnay',
+	dishPrice: 120
+},
+{
+	dishName: 'DANZANTE Pinot Grigio',
+	dishPrice: 120
+},
+{
+	dishName: 'CHABLIS Ropiteau',
+	dishPrice: 190
+},
+{
+	dishName: 'CONTENDA Cabernet Sauvignon',
+	dishPrice: 32
+},
+{
+	dishName: 'CANEPA Classico Carmenere',
+	dishPrice: 14
+},
+{
+	dishName: 'ROPITEAU Grenache/Syrach',
+	dishPrice: 15
+},
+{
+	dishName: 'RACCOLATO Appassimento',
+	dishPrice: 120
+},
+{
+	dishName: 'IGT Veneto - Corvina, Molinara, Rondinella, Lagrain, Merlot – dry –ITALY',
+	dishPrice: 120
+},
+{
+	dishName: 'LES EYQUEMS Chateaux',
+	dishPrice: 130
+},
+{
+	dishName: 'CANEPA Famiglia Reserva Cabernet Sauvignon',
+	dishPrice: 130
+},
+{
+	dishName: 'ANIMA de RAIMAT Tinto',
+	dishPrice: 140
+}
+];
